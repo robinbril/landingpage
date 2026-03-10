246e64db1644728821a08f5dc08728a4832e32a6
Robin Bril <robin.bril@gmail.com>
2026-03-10 16:49:07 +0100

    chore: remove GitHub Pages config for Vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/landingpage',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  assetPrefix: '/landingpage/',
-  turbopack: {
-    root: process.cwd(),
-  },
 }
 
 module.exports = nextConfig;
